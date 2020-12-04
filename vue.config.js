72a1c44c1a8d7a68a963e7f8a87496c8751ec761
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-04 09:22:47 +0100

    fix path

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/coopgoods/'
-    : '/',
   lintOnSave: false,
   configureWebpack: {
     // Set up all the aliases we use in our app.
